b36e9fe09e3beb2d691a0f1c81f1185e4517fdea
AbdElrhman Mohamed <AbdElRhman.m056@gmail.com>
2019-03-08 23:19:38 +0200

    features: adding extract link feature

--- web_crawler.py
+++ web_crawler.py
@@ -5,3 +5,13 @@ def get_page(url):
         return urllib.urlopen(url).read()
     except:
         return ""
+
+def get_next_target(page):
+	""" takes a page as an input and returns a link in this page"""
+    start_link = page.find('<a href=')
+    if start_link == -1:
+        return None, 0
+    start_quote = page.find('"', start_link)
+    end_quote = page.find('"', start_quote + 1)
+    url = page[start_quote + 1:end_quote]
+    return url, end_quote
